80cadbf737e3d42d95872cbc5300257d8c8c8d6c
tripm1405 <tri.pm.1405@gmail.com>
2023-03-23 16:33:25 +0700

    add product-api for home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,30 +2,33 @@ import React, { useState, useEffect } from "react";
 
 import Slide from "~/components/Slide";
 import ProductGrid from "~/components/ProductGrid";
-import { getProductAPI } from '~/api/product.api';
+import { getProductWithBody } from '~/api/product.api';
 
 const Home = () => {
   const [manga, setManga] = useState([]);
   const [figure, setFigure] = useState([]);
 
   async function getProductsHandler() {
-    const data = await getProductAPI();
+    try {
+      const [mangaData, figureData] = await Promise.all([getProductWithBody({ Type: false, Take: 12 }), getProductWithBody({ Type: true, Take: 12 })])
+  
+      setManga(mangaData);
+      setFigure(figureData);
+    }
+    catch {
+      setManga(Array(12).fill({
+        "meta": "",
+        "name": "Boku girl",
+        "srcImg": "./Uploads/manga/1.png",
+        "price": 20000
+      }));
 
-    setManga(data.filter(e => !e.type).map(e => ({ ...e, srcImg: e.image })));
-    setFigure(data.filter(e => e.type).map(e => ({ ...e, srcImg: e.image })));
-
-    // setManga(Array(12).fill({
-    //   "meta": "",
-    //   "name": "Boku girl",
-    //   "srcImg": "./Uploads/manga/1.png",
-    //   "price": 20000
-    // }));
-
-    // setFigure(Array(12).fill({
-    //   "meta": "",
-    //   "srcImg": "/Uploads/figure/1.png",
-    //   "price": 20000
-    // }));
+      setFigure(Array(12).fill({
+        "meta": "",
+        "srcImg": "/Uploads/figure/1.png",
+        "price": 20000
+      }));
+    }
   }
 
   useEffect(() => {
