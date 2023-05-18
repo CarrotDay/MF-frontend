2362aa330c26ab2319142428c6196284f433e2e4
tripm1405 <tri.pm.1405@gmail.com>
2023-05-18 23:07:35 +0700

    fix

--- src/components/AnnounceList.jsx
+++ src/components/AnnounceList.jsx
@@ -7,17 +7,19 @@ import moment from "moment";
 import { getAnnounces } from '~/api/announce.api';
 
 const AnnounceList = () => {
-  const [announces, setAnnounces] = useState([]);
+  const [announces, setAnnounces] = useState(null);
   
   useQuery({
     queryKey: ['announces'],
     queryFn: getAnnounces,
     onSuccess: data => {
-      setAnnounces(data?.data?.map(e => ({
-        ...e,
-        content: e?.content?.substring(0, 60) + '...',
-        createAt: moment(e?.createAt).format('YYYY/MM/DD HH:mm')
-      })));
+      if (data?.data) {
+        setAnnounces(data?.data?.map(e => ({
+          ...e,
+          content: e?.content?.substring(0, 60) + '...',
+          createAt: moment(e?.createAt).format('YYYY/MM/DD HH:mm')
+        })));
+      }
     }
   });
 
@@ -28,21 +30,23 @@ const AnnounceList = () => {
           <div className="title-list col-12 px-3">
             <h1 className="title-announce font-weight-bold">Tin tức</h1>
           </div>
-          {announces?.map(e => (
-            <div className="items-announces col-12" key={e.id}>
-              <Link to={`/announce/${e.meta}`} className="item-announce row my-2 py-3">
-                <div className="img-announce col-4">
-                  <img src={e.image} alt=""/>
-                </div>
+          {announces ? (
+            announces?.map(e => (
+              <div className="items-announces col-12" key={e.id}>
+                <Link to={`/announce/${e.meta}`} className="item-announce row my-2 py-3">
+                  <div className="img-announce col-4">
+                    <img src={e.image} alt=""/>
+                  </div>
 
-                <div className="content-announce col-8">
-                  <h4 className="title-item-announce">{e.title}</h4>
-                  <p className="content-announce">{e.content}</p>
-                  <p className="create_date_announce">{e.createAt}</p>
-                </div>
-              </Link>
-            </div>
-          ))}
+                  <div className="content-announce col-8">
+                    <h4 className="title-item-announce">{e.title}</h4>
+                    <p className="content-announce">{e.content}</p>
+                    <p className="create_date_announce">{e.createAt}</p>
+                  </div>
+                </Link>
+              </div>
+            ))
+          ) : "Chưa có thông báo nào"}
         </div>
         <div className="calendar-announce col-lg-4 ml-lg-3 d-md-block d-none p-0">
           <div className="title-list">
